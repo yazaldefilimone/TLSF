a1bf56ec70d5162417826abe7bc79dbb68f0d6e3
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-10 11:12:18 +0200

    *

--- src/block.rs
+++ src/block.rs
@@ -1,33 +1,6 @@
-use std::ptr::NonNull;
-
-pub struct Block {
+pub struct BlockHeader {
   pub size: usize,
-  pub offset: usize,
-  pub next_free: Option<NonNull<Block>>,
-  pub prev_free: Option<NonNull<Block>>,
-  pub next_physical: Option<NonNull<Block>>,
-  pub prev_physical: Option<NonNull<Block>>,
-}
-
-pub struct BlockMap {
-  pub bin_idx: usize,
-  pub sub_bin_idx: usize,
-  pub rounded_size: usize,
-  pub idx: usize,
-}
-impl Block {
-  pub fn is_free(&self) -> bool {
-    self.prev_free.is_none()
-  }
-
-  pub fn mark_used(&mut self, adjustment: usize) {
-    self.prev_free = Some(NonNull::new(self).unwrap());
-    self.next_free = Some(NonNull::new(self as *mut _).unwrap());
-    self.offset += adjustment;
-  }
-
-  pub fn mark_free(&mut self) {
-    self.prev_free = None;
-    self.next_free = None;
-  }
+  pub free: bool,
+  pub next_free: Option<Box<BlockHeader>>,
+  pub prev_free: Option<Box<BlockHeader>>,
 }
